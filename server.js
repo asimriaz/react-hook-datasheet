07d252a6db609013c3348c78badb07d4b9fb2586
asimriaz <>
2021-12-12 18:13:47 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api/students/', require('./api/student'));
+app.use('/api/courses/', require('./api/course'));
+app.use('/api/registrations/', require('./api/registration'));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(5000, () => {
+	console.log(`Server is running on http://localhost:5000/`);
+});
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DataSheet from './components/DataSheet';
+
+function App() {
+  return (
+    <div className="App">
+        <DataSheet />
+    </div>
+  );
+}
+
+export default App;
